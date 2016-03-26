79a03466402eb94b0002594391ee761e0e9f9cc2
david o grady <dogrady92@gmail.com>
2016-03-26 17:39:38 +0000

    added user authentification with firebase

--- MovieApp/app.js
+++ MovieApp/app.js
@@ -1,31 +1,85 @@
-var moviesApp = angular.module('movieCatApp', ['ngRoute']);
-
+var moviesApp = angular.module('movieCatApp', ['firebase','ngRoute']);
+moviesApp.constant('FBMSG','https://reviewitproject.firebaseio.com/movies');
 
     moviesApp.config(['$routeProvider',
       function($routeProvider) {
         $routeProvider
           .when('/movies', {
             templateUrl: 'partials/movie-list.html',
-            controller: 'movieListCtrl'
+            controller: 'movieListCtrl',
+              resolve: {
+              "currentAuth" : ["authFactory", function(authFactory){
+                var auth = authFactory.auth();
+                return auth.$requireAuth();
+              }]
+            }
           })
           .when('/movies-info', {
             templateUrl: 'partials/movie-list-info.html',
-            controller: 'movieListCtrl'
+            controller: 'movieListCtrl',
+              resolve: {
+              "currentAuth" : ["authFactory", function(authFactory){
+                var auth = authFactory.auth();
+                return auth.$requireAuth();
+              }]
+            }
           })
-           .when('/', {
+           .when('/logout', {
+            templateUrl: 'partials/movie-list-info.html',
+            templateUrl: 'partials/login.html',
+            controller: 'loginCtrl',
+            controllerAs: 'loginCtl',
+            resolve: {
+              "logout": ['authFactory', function(authFactory){
+                authFactory.logout();
+              }]
+            }
+          })
+            .when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'loginCtrl',
+            controllerAs: 'loginCtl'
+          })
+
+              .when('/', {
             templateUrl: 'partials/front-page.html'
+          })
+            .when('/register', {
+            templateUrl: 'partials/register.html',
+            controller: 'loginCtrl',
+            controllerAs: 'loginCtl'
           })
             .when('/about', {
             templateUrl: 'partials/about.html'
           })
           .when('/movies/:movieId', {
             templateUrl: 'partials/movie-detail.html',
-            controller: 'movieDetailCtrl'
+            controller: 'movieDetailCtrl',
+               resolve: {
+              "currentAuth" : ["authFactory", function(authFactory){
+                var auth = authFactory.auth();
+                return auth.$requireAuth();
+              }]
+            }
           })
           .otherwise({
             redirectTo: '/movies'
           });
-      }]);
+      }])
+
+.run(['$rootScope', '$location', function($rootScope, $location){
+
+  $rootScope.$on('$routeChangeError', function(event, next, previous, error){
+    console.log(error);
+      if(error = 'AUTH_REQUIRED'){
+
+        console.log('Error in Auth');
+        $location.path("/login");
+    }
+  })
+}]);
+
+
 
     moviesApp.controller('movieListCtrl', ['$scope', 'movieService',
           function($scope, movieService) {
@@ -44,6 +98,32 @@ var moviesApp = angular.module('movieCatApp', ['ngRoute']);
         }
              $scope.orderProp = 'age';
 
+          }]) 
+
+        moviesApp.controller('loginCtrl', ['FBMSG','authFactory','$window',
+          function(FBMSG,authFactory,$window) {
+              var self = this;
+              self.signUp = function(){
+                var result = authFactory.createUser(self.email, self.password);
+
+                result.then(function(userData){
+                  console.log("User created successfully with uid: ", userData.uid);
+                },function(error){
+                  console.log(error);
+                })
+              }
+
+                 self.login = function(){
+                var result = authFactory.authUser(self.email, self.password);
+
+                result.then(function(userData){
+                  console.log("User logged in successfully with uid: ", userData.uid);
+                  $window.location.href = '/';
+                },function(error){
+                  console.log(error);
+                })
+              }
+         
           }]) 
 
    moviesApp.controller('movieDetailCtrl', 
@@ -82,4 +162,39 @@ var moviesApp = angular.module('movieCatApp', ['ngRoute']);
             }
             return api
         }])
+
+
+          moviesApp.factory('authFactory',['FBMSG','$firebaseAuth',function(FBMSG, $firebaseAuth){
+            var authFactory = {};
+            var ref = new Firebase(FBMSG);
+            var auth = $firebaseAuth(ref);
+
+            authFactory.createUser = function(email,password){
+            return auth.$createUser({
+              email: email,
+              password: password
+            })};
+
+               authFactory.authUser = function(email,password){
+            return auth.$authWithPassword({
+              email: email,
+              password: password
+            })};
+            
+
+            authFactory.auth = function(){
+              return auth;
+            }
+
+            authFactory.logout = function(){
+              auth.$unauth();
+            }
+
+
+            return authFactory;
+          
+          }])
+
+
+
 
